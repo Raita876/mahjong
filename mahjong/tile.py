0bd3f0dec778e6ed6f827608e8a46bce290db1a7
Raita Watanabe <watanaberit20171129@gmail.com>
2019-01-27 23:13:46 +0900

    Implement create_tiles_tenpai()

--- mahjong/tile.py
+++ mahjong/tile.py
@@ -1,3 +1,6 @@
+import collections
+import random
+
 import yaml
 
 
@@ -33,3 +36,73 @@ class TilesHandler:
             tiles_in_char += tile.char
 
         return tiles_in_char
+
+
+def create_tiles_tenpai():
+    tiles = mentsu() + mentsu() + mentsu() + mentsu() + janto()
+
+    tiles = random.sample(tiles, 13)
+
+    if is_reasonable_tiles_num(tiles):
+        return tiles
+    else:
+        return create_tiles_tenpai()
+
+
+def mentsu():
+    mentsu_type_choices = ["shuntsu", "kotsu"]
+    mentsu_type = random.choice(mentsu_type_choices)
+
+    if mentsu_type == "shuntsu":
+        return shuntsu()
+    elif mentsu_type == "kotsu":
+        return kotsu()
+
+
+def shuntsu():
+    tiles_all = get_tiles_for_shuntsu()
+    selected_tile = random.choice(tiles_all)
+
+    return [Tile(selected_tile.tile_type, selected_tile.num + i) for i in range(3)]
+
+
+def kotsu():
+    tiles_all = get_tiles_all()
+    selected_tile = random.choice(tiles_all)
+
+    return [selected_tile for i in range(3)]
+
+
+def janto():
+    tiles_all = get_tiles_all()
+    selected_tile = random.choice(tiles_all)
+
+    return [selected_tile for i in range(2)]
+
+
+def get_tiles_all():
+    tiles_m = [Tile("m", i) for i in range(1, 10)]
+    tiles_p = [Tile("p", i) for i in range(1, 10)]
+    tiles_s = [Tile("s", i) for i in range(1, 10)]
+    tiles_z = [Tile("z", i) for i in range(1, 8)]
+
+    return tiles_m + tiles_p + tiles_s + tiles_z
+
+
+def get_tiles_for_shuntsu():
+    tiles_m = [Tile("m", i) for i in range(1, 8)]
+    tiles_p = [Tile("p", i) for i in range(1, 8)]
+    tiles_s = [Tile("s", i) for i in range(1, 8)]
+
+    return tiles_m + tiles_p + tiles_s
+
+
+def is_reasonable_tiles_num(tiles):
+    handler = TilesHandler(tiles)
+    tiles_in_char = handler.get_tiles_in_char()
+    c = collections.Counter(tiles_in_char)
+
+    if c[max(c)] < 5:
+        return True
+    else:
+        return False
